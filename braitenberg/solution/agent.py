7c6a34ebb6486142e92168a39d498028fb1aa431
Dave Whipps <dwhipps@me.com>
2021-10-08 08:46:30 -0400

    Solutions for exercises 1 and 2.

--- braitenberg/solution/agent.py
+++ braitenberg/solution/agent.py
@@ -26,8 +26,8 @@ from preprocessing import preprocess
 # TODO edit this Config class ! Play with different gain and const values
 @dataclass
 class BraitenbergAgentConfig:
-    gain: float = 0.9
-    const: float = 0.0
+    gain: float = 0.8
+    const: float = 0.1
 
 
 class BraitenbergAgent:
--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -4,13 +4,43 @@ import numpy as np
 
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
-    return res
+    return get_motor_left_matrix_fear(shape)
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    res[100:150, 100:300] = -1
+    return get_motor_right_matrix_fear(shape)
+
+# FEAR
+def get_motor_left_matrix_fear(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], 0:int(shape[1]/2)] = 1
+    return res
+
+def get_motor_right_matrix_fear(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], int(shape[1]/2 +1):shape[1]] = 1
+    return res
+
+# EXPLORER
+def get_motor_left_matrix_explorer(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], int(shape[1]/2 +1):shape[1]] = -1
+    return res
+
+def get_motor_right_matrix_explorer(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], 0:int(shape[1]/2)] = -1
+    return res
+
+# MODIFIED EXPLORER
+def get_motor_left_matrix_explorer_2(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], int(shape[1]/2 +1):shape[1]] = -1
+    res[0:shape[0], 0:int(shape[1]/2)] = 0.5
+    return res
+
+def get_motor_right_matrix_explorer_2(shape: Tuple[int, int]) -> np.ndarray:
+    res = np.zeros(shape=shape, dtype="float32")
+    res[0:shape[0], 0:int(shape[1]/2)] = -1
+    res[0:shape[0], int(shape[1]/2 +1):shape[1]] = 0.5
     return res
