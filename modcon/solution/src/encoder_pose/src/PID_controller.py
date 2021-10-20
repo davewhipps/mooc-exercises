ebe8159fff7792ea33d086aee58870de0579e7d5
Dave Whipps <dwhipps@me.com>
2021-10-19 20:32:39 -0400

    Solutions for modcon.

--- modcon/solution/src/encoder_pose/src/PID_controller.py
+++ modcon/solution/src/encoder_pose/src/PID_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[3]:
 
 
 import numpy as np
@@ -30,10 +30,23 @@ def PIDController(v_0, theta_ref, theta_hat, prev_e, prev_int, delta_t):
         e_int (:double:) current integral error (automatically becomes prev_int_y at next iteration).
     """
     
-    # TODO: these are random values, you have to implement your own PID controller in here
-    omega = np.random.uniform(-8.0, 8.0)
-    e = np.random.random()
-    e_int = np.random.random()
+    #Error
+    e = theta_ref - theta_hat
+    
+    #Error integral
+    e_int = prev_int + e*delta_t
+    
+    # anti-windup - preventing the integral error from growing too much?
+    e_int = max(min(e_int,2),-2)
+    
+    #Error derivative
+    e_der = (e - prev_e)/delta_t
+    
+    #Constants (to tweak)
+    k_p = 5
+    k_i = 0.2
+    k_d = 0.1
+    
+    omega = k_p*e + k_i*e_int + k_d*e_der
     
     return [v_0, omega], e, e_int
-
